a566ef7e8650e89e893181e56df37bf2a1ddac40
Wilmer Gomez <wilmergomez010@gmail.com>
2025-02-22 12:28:25 -0500

    Ya se puede ejecutar en segundo plano, pero falta que se ejecute al principio de iniciar windows

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from datetime import time
+from datetime import datetime
+from tkinter import messagebox
 
-hora_actual = time()
-hora_dada = time(12, 30)
+def main():
+    while True:
 
-print("Hora: ", hora_dada.hour)
+        hora_actual = datetime.now()
+        hora_hora_minuto = str(hora_actual.hour)+ ":" + str(hora_actual.minute)
+        hora_dada_hora_minuto = "12:20"
+
+        if hora_hora_minuto == hora_dada_hora_minuto:
+            messagebox.showinfo(message= f"DEBES IRTE YA ES LA HORA DEL ALMUERZO: {hora_dada_hora_minuto} PM")
+            break
+        else:
+            pass
+
+
+if __name__ == "__main__":
+    main()
