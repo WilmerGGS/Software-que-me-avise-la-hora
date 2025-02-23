be060e281ab2a4adc135b9621399861d43b49769
Wilmer Gomez <wilmergomez010@gmail.com>
2025-02-23 10:59:24 -0500

    Cambios hechos v1.01

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from datetime import datetime
 from tkinter import messagebox
 from tkinter import Tk
+import time
 
 def main():
     while True:
 
         hora_actual = datetime.now()
         hora_hora_minuto = str(hora_actual.hour)+ ":" + str(hora_actual.minute)
-        hora_dada_hora_minuto = "19:00"
+        hora_dada_hora_minuto = "12:25"
 
         if hora_hora_minuto == hora_dada_hora_minuto:
             root = Tk()
             root.withdraw()
             messagebox.showinfo(message= f"DEBES IRTE YA ES LA HORA DEL ALMUERZO: {hora_dada_hora_minuto} PM")
+            root.destroy()
             break
         else:
-            pass
+            time.sleep(20)  # Verificar cada 20 segundos
 
 
 if __name__ == "__main__":
