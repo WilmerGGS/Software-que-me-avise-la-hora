abbd831699b8ba376f803760921afbec42d3b286
Wilmer Gomez <wilmergomez010@gmail.com>
2025-02-22 19:03:44 -0500

    Falta acomodar El while True para que no gaste muchos recursos, y cuando se inicie windows se ejecute automaticamente el script

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from datetime import datetime
 from tkinter import messagebox
+from tkinter import Tk
 
 def main():
     while True:
 
         hora_actual = datetime.now()
         hora_hora_minuto = str(hora_actual.hour)+ ":" + str(hora_actual.minute)
-        hora_dada_hora_minuto = "12:20"
+        hora_dada_hora_minuto = "19:00"
 
         if hora_hora_minuto == hora_dada_hora_minuto:
+            root = Tk()
+            root.withdraw()
             messagebox.showinfo(message= f"DEBES IRTE YA ES LA HORA DEL ALMUERZO: {hora_dada_hora_minuto} PM")
             break
         else:
