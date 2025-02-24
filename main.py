f41eedec7835d64d92965aec52a199ef07b3fca3
Wilmer Gomez <wilmergomez010@gmail.com>
2025-02-24 10:25:56 -0500

    Modularizamos el codigo

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ from tkinter import messagebox
 from tkinter import Tk
 import time
 
-def main():
+def saber_hora():
     while True:
-
         hora_actual = datetime.now()
         hora_hora_minuto = str(hora_actual.hour)+ ":" + str(hora_actual.minute)
-        hora_dada_hora_minuto = "12:25"
-
+        hora_dada_hora_minuto = "10:23" #Aqui puedes controlar la hora que quieres que se active la alarma
         if hora_hora_minuto == hora_dada_hora_minuto:
-            root = Tk()
-            root.withdraw()
-            messagebox.showinfo(message= f"DEBES IRTE YA ES LA HORA DEL ALMUERZO: {hora_dada_hora_minuto} PM")
-            root.destroy()
+            main_ventana(hora_dada_hora_minuto)
             break
         else:
             time.sleep(20)  # Verificar cada 20 segundos
 
+def main_ventana(hora_dada_hora_minuto):
+    root = Tk()
+    root.withdraw()
+    messagebox.showinfo(message= f"DEBES IRTE YA ES LA HORA DEL ALMUERZO: {hora_dada_hora_minuto} PM")
+    root.destroy()
+
 
 if __name__ == "__main__":
-    main()
+    saber_hora()
